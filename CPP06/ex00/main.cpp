10d135cdfc749328c03e15b961ee8090e61043ca
IKyllian <kyllian.delporte@gmail.com>
2021-12-10 14:34:07 +0100

    continue CPP06

--- CPP06/ex00/main.cpp
+++ CPP06/ex00/main.cpp
@@ -7,11 +7,24 @@
 int main(int argc, char **argv)
 {
 	double nbr;
+	std::string str;
 
 	if (argc == 2)
 	{
-		nbr = atof(argv[1]);
+		
+		str = argv[1];
+		// is_inf = ((str == "inf" == 0) || str == "inff" == 0) || str == "+inff" == 0) || str == "inf" == 0) || str == "inf" == 0) || );
+		// if (str.length() != 1 && !(str[0] >= 48 && str[0] <= 57))
+		// {
+		// 	std::cout << "Syntax Error" << std::endl;
+		// 	return (1);
+		// }
+		// if (str.length() == 1 && !((str[0] >= 0 && str[0] <= 32) || (str[0] >= 48 && str[0] <= 57)))
+		// 	nbr = static_cast<double>(argv[1][0]);
+		// else
+			nbr = atof(argv[1]);
 
+		// ********* To char *********
 		if (isinf(nbr) || isnan(nbr) || nbr > CHAR_MAX || nbr < CHAR_MIN)
 			std::cout << "char: impossible" << std::endl;
 		else if (std::isprint(static_cast<char>(nbr)))
@@ -19,20 +32,26 @@ int main(int argc, char **argv)
 		else
 			std::cout << "char: Non Displayable" << std::endl;
 
+		// ********* To int *********
+		int i_nb = static_cast<int>(nbr);
 		if (isinf(nbr) || isnan(nbr) || nbr > INT_MAX || nbr < INT_MIN)
 			std::cout << "int: impossible" << std::endl;
 		else
-			std::cout << "int: " << static_cast<int>(nbr) << std::endl;
+			std::cout << "int: " << i_nb << std::endl;
 
-		if (isinf(nbr) || isnan(nbr) || nbr > FLT_MAX || nbr < FLT_MIN)
-			std::cout << "float: impossible" << std::endl;
+		// ********* To float *********
+		float f_nb = static_cast<float>(nbr);
+		if (nbr - f_nb == 0)
+			std::cout << "float: " << f_nb << ".0f" << std::endl;
 		else
-			std::cout << "float: " << static_cast<float>(nbr) << "f" << std::endl;
+			std::cout << "float: " << f_nb << "f" << std::endl;
 
-		if (isinf(nbr) || isnan(nbr) || nbr > DBL_MAX || nbr < DBL_MIN)
-			std::cout << "double: impossible" << std::endl;
+		// ********* To double *********
+		double d_nb = static_cast<double>(nbr);
+		if (nbr - f_nb == 0)
+			std::cout << "double: " << d_nb << ".0" << std::endl;
 		else
-			std::cout << "double: " << static_cast<double>(nbr) << std::endl;
+			std::cout << "double: " << d_nb << std::endl;
 	}
 	else
 		std::cout << "Error : Argument" << std::endl;
